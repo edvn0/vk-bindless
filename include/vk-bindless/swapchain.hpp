f065c429b756c5945e8a4653a2dd80dd92e38a01
Edwin Dahlberg <edwin98dahlberg@gmail.com>
2025-08-12 23:39:11 +0200

    feat: This worked

--- include/vk-bindless/swapchain.hpp
+++ include/vk-bindless/swapchain.hpp
@@ -1,43 +1,48 @@
 #pragma once
 
+#include "vk-bindless/expected.hpp"
 #include "vk-bindless/forward.hpp"
 #include "vk-bindless/handle.hpp"
 
 #include <array>
 #include <cstdint>
 #include <string>
+#include <vulkan/vulkan.h>
 
 namespace VkBindless {
 
-  enum class SwapchainPresentFailure {
+enum class SwapchainPresentFailure
+{
   OutOfDate,
   Suboptimal,
 };
 
-class Swapchain final {
+class Swapchain final
+{
   static constexpr auto max_swapchain_images = 8U;
 
 public:
-  Swapchain(IContext &context, std::uint32_t width, std::uint32_t height);
+  Swapchain(IContext& context, std::uint32_t width, std::uint32_t height);
   ~Swapchain();
 
-  Swapchain(const Swapchain &) = delete;
-  auto operator=(const Swapchain &) -> Swapchain & = delete;
-  Swapchain(Swapchain &&) = delete;
-  auto operator=(Swapchain &&) -> Swapchain & = delete;
+  Swapchain(const Swapchain&) = delete;
+  auto operator=(const Swapchain&) -> Swapchain& = delete;
+  Swapchain(Swapchain&&) = delete;
+  auto operator=(Swapchain&&) -> Swapchain& = delete;
 
-  auto present(VkSemaphore wait_semaphore) -> Expected<void, SwapchainPresentFailure>;
-  auto resize(std::uint32_t,std::uint32_t) -> void;
+  auto present(VkSemaphore wait_semaphore)
+    -> Expected<void, SwapchainPresentFailure>;
+  auto resize(std::uint32_t, std::uint32_t) -> void;
 
   auto current_vk_image() const -> VkImage;
   auto current_vk_image_view() const -> VkImageView;
   auto current_texture() -> TextureHandle;
 
-  auto surface_format() const -> const VkSurfaceFormatKHR &;
+  auto surface_format() const -> const VkSurfaceFormatKHR&;
   auto swapchain_current_image_index() const -> std::uint32_t;
   auto swapchain_image_count() const -> std::uint32_t;
 
-  auto context() -> IContext &;
+  auto context() -> IContext&;
   auto graphics_queue() const -> VkQueue;
   auto width() const -> std::uint32_t { return swapchain_width; }
   auto height() const -> std::uint32_t { return swapchain_height; }
@@ -49,7 +54,7 @@ public:
   auto set_next_image_needed(bool value) -> void;
 
 private:
-  Context &context_ref;
+  Context& context_ref;
   VkQueue graphics_queue_handle = VK_NULL_HANDLE;
   std::uint32_t swapchain_width = 0;
   std::uint32_t swapchain_height = 0;
@@ -59,8 +64,8 @@ private:
   bool need_next_image = true;
   VkSwapchainKHR swapchain_khr = VK_NULL_HANDLE;
   VkSurfaceFormatKHR swapchain_surface_format{
-      .format = VK_FORMAT_UNDEFINED,
-      .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR,
+    .format = VK_FORMAT_UNDEFINED,
+    .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR,
   };
   std::array<TextureHandle, max_swapchain_images> swapchain_textures{};
   std::array<VkSemaphore, max_swapchain_images> acquire_semaphores{};
--- src/swapchain.cpp
+++ src/swapchain.cpp
@@ -10,13 +10,15 @@
 namespace VkBindless {
 
 namespace {
-auto choose_swap_surface_format(const std::vector<VkSurfaceFormatKHR> &formats,
-                                ColorSpace requestedColorSpace,
-                                bool hasSwapchainColorspaceExt) {
+auto
+choose_swap_surface_format(const std::vector<VkSurfaceFormatKHR>& formats,
+                           ColorSpace requestedColorSpace,
+                           bool hasSwapchainColorspaceExt)
+{
 
   auto is_native_swapchain_bgr =
-      [](const std::vector<VkSurfaceFormatKHR> &formats) -> bool {
-    for (const VkSurfaceFormatKHR &fmt : formats) {
+    [](const std::vector<VkSurfaceFormatKHR>& formats) -> bool {
+    for (const VkSurfaceFormatKHR& fmt : formats) {
       // The preferred format should be the one which is closer to the beginning
       // of the formats container. If BGR is encountered earlier, it should be
       // picked as the format of choice. If RGB happens to be earlier, take it.
@@ -35,45 +37,47 @@ auto choose_swap_surface_format(const std::vector<VkSurfaceFormatKHR> &formats,
   };
 
   auto colorSpaceToVkSurfaceFormat =
-      [](ColorSpace colorSpace, bool isBGR,
-         bool hasSwapchainColorspaceExt) -> VkSurfaceFormatKHR {
+    [](ColorSpace colorSpace,
+       bool isBGR,
+       bool hasSwapchainColorspaceExt) -> VkSurfaceFormatKHR {
     switch (colorSpace) {
-    case ColorSpace::SRGB_NONLINEAR:
-      return VkSurfaceFormatKHR{isBGR ? VK_FORMAT_B8G8R8A8_UNORM
-                                      : VK_FORMAT_R8G8B8A8_UNORM,
-                                VK_COLOR_SPACE_SRGB_NONLINEAR_KHR};
-    case ColorSpace::SRGB_EXTENDED_LINEAR:
-      if (hasSwapchainColorspaceExt)
-        return VkSurfaceFormatKHR{VK_FORMAT_R16G16B16A16_SFLOAT,
-                                  VK_COLOR_SPACE_EXTENDED_SRGB_LINEAR_EXT};
-      [[fallthrough]];
-    case ColorSpace::HDR10:
-      if (hasSwapchainColorspaceExt) {
-        return VkSurfaceFormatKHR{isBGR ? VK_FORMAT_A2B10G10R10_UNORM_PACK32
-                                        : VK_FORMAT_A2R10G10B10_UNORM_PACK32,
-                                  VK_COLOR_SPACE_HDR10_ST2084_EXT};
-      }
-      [[fallthrough]];
-    default:
-      // default to normal sRGB non linear.
-      return VkSurfaceFormatKHR{isBGR ? VK_FORMAT_B8G8R8A8_SRGB
-                                      : VK_FORMAT_R8G8B8A8_SRGB,
-                                VK_COLOR_SPACE_SRGB_NONLINEAR_KHR};
+      case ColorSpace::SRGB_NONLINEAR:
+        return VkSurfaceFormatKHR{ isBGR ? VK_FORMAT_B8G8R8A8_UNORM
+                                         : VK_FORMAT_R8G8B8A8_UNORM,
+                                   VK_COLOR_SPACE_SRGB_NONLINEAR_KHR };
+      case ColorSpace::SRGB_EXTENDED_LINEAR:
+        if (hasSwapchainColorspaceExt)
+          return VkSurfaceFormatKHR{ VK_FORMAT_R16G16B16A16_SFLOAT,
+                                     VK_COLOR_SPACE_EXTENDED_SRGB_LINEAR_EXT };
+        [[fallthrough]];
+      case ColorSpace::HDR10:
+        if (hasSwapchainColorspaceExt) {
+          return VkSurfaceFormatKHR{ isBGR ? VK_FORMAT_A2B10G10R10_UNORM_PACK32
+                                           : VK_FORMAT_A2R10G10B10_UNORM_PACK32,
+                                     VK_COLOR_SPACE_HDR10_ST2084_EXT };
+        }
+        [[fallthrough]];
+      default:
+        // default to normal sRGB non linear.
+        return VkSurfaceFormatKHR{ isBGR ? VK_FORMAT_B8G8R8A8_SRGB
+                                         : VK_FORMAT_R8G8B8A8_SRGB,
+                                   VK_COLOR_SPACE_SRGB_NONLINEAR_KHR };
     }
   };
 
-  const VkSurfaceFormatKHR preferred = colorSpaceToVkSurfaceFormat(
-      requestedColorSpace, is_native_swapchain_bgr(formats),
-      hasSwapchainColorspaceExt);
+  const VkSurfaceFormatKHR preferred =
+    colorSpaceToVkSurfaceFormat(requestedColorSpace,
+                                is_native_swapchain_bgr(formats),
+                                hasSwapchainColorspaceExt);
 
-  for (const VkSurfaceFormatKHR &fmt : formats) {
+  for (const VkSurfaceFormatKHR& fmt : formats) {
     if (fmt.format == preferred.format &&
         fmt.colorSpace == preferred.colorSpace) {
       return fmt;
     }
   }
 
-  for (const VkSurfaceFormatKHR &fmt : formats) {
+  for (const VkSurfaceFormatKHR& fmt : formats) {
     if (fmt.format == preferred.format) {
       return fmt;
     }
@@ -83,33 +87,53 @@ auto choose_swap_surface_format(const std::vector<VkSurfaceFormatKHR> &formats,
 }
 } // namespace
 
-auto Swapchain::swapchain_image_count() const -> std::uint32_t {
+auto
+Swapchain::swapchain_image_count() const -> std::uint32_t
+{
   return image_count;
 }
 
-auto Swapchain::context() -> IContext & { return context_ref; }
+auto
+Swapchain::context() -> IContext&
+{
+  return context_ref;
+}
 
-auto Swapchain::current_texture() -> TextureHandle {
-  auto &vulkan_context = static_cast<Context &>(context_ref);
+auto
+Swapchain::current_texture() -> TextureHandle
+{
+  auto& vulkan_context = static_cast<Context&>(context_ref);
 
   if (need_next_image) {
     if (present_fences[swapchain_current_image_index()]) {
-      // VK_EXT_swapchain_maintenance1: before acquiring again, wait for the presentation operation to finish
-      VK_VERIFY(vkWaitForFences(context_ref.vkb_device, 1, &present_fences[swapchain_current_image_index()], VK_TRUE, UINT64_MAX));
-      VK_VERIFY(vkResetFences(context_ref.vkb_device, 1, &present_fences[swapchain_current_image_index()]));
+      // VK_EXT_swapchain_maintenance1: before acquiring again, wait for the
+      // presentation operation to finish
+      VK_VERIFY(
+        vkWaitForFences(context_ref.vkb_device,
+                        1,
+                        &present_fences[swapchain_current_image_index()],
+                        VK_TRUE,
+                        UINT64_MAX));
+      VK_VERIFY(
+        vkResetFences(context_ref.vkb_device,
+                      1,
+                      &present_fences[swapchain_current_image_index()]));
     }
     const VkSemaphoreWaitInfo wait_info = {
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_WAIT_INFO,
-        .pNext = nullptr,
-        .flags = 0,
-        .semaphoreCount = 1,
-        .pSemaphores = &vulkan_context.timeline_semaphore,
-        .pValues = &timeline_wait_values[swapchain_image_index],
+      .sType = VK_STRUCTURE_TYPE_SEMAPHORE_WAIT_INFO,
+      .pNext = nullptr,
+      .flags = 0,
+      .semaphoreCount = 1,
+      .pSemaphores = &vulkan_context.timeline_semaphore,
+      .pValues = &timeline_wait_values[swapchain_image_index],
     };
     vkWaitSemaphores(vulkan_context.get_device(), &wait_info, UINT64_MAX);
     VkSemaphore acquireSemaphore = acquire_semaphores[swapchain_image_index];
-    vkAcquireNextImageKHR(vulkan_context.get_device(), swapchain_khr,
-                          UINT64_MAX, acquireSemaphore, VK_NULL_HANDLE,
+    vkAcquireNextImageKHR(vulkan_context.get_device(),
+                          swapchain_khr,
+                          UINT64_MAX,
+                          acquireSemaphore,
+                          VK_NULL_HANDLE,
                           &swapchain_image_index);
     need_next_image = false;
     vulkan_context.immediate_commands->wait_semaphore(acquireSemaphore);
@@ -122,53 +146,62 @@ auto Swapchain::current_texture() -> TextureHandle {
   return TextureHandle{};
 }
 
-auto Swapchain::current_vk_image() const -> VkImage {
+auto
+Swapchain::current_vk_image() const -> VkImage
+{
   if (swapchain_current_image_index() < image_count) {
-    auto *tex = context_ref.texture_pool
-                    .get(swapchain_textures.at(swapchain_current_image_index()))
-                    .value();
+    auto* tex = context_ref.texture_pool
+                  .get(swapchain_textures.at(swapchain_current_image_index()))
+                  .value();
     return tex->get_image();
   }
   return VK_NULL_HANDLE;
 }
 
-auto Swapchain::current_vk_image_view() const -> VkImageView {
+auto
+Swapchain::current_vk_image_view() const -> VkImageView
+{
   if (swapchain_current_image_index() < image_count) {
-    auto *tex = context_ref.texture_pool
-                    .get(swapchain_textures.at(swapchain_current_image_index()))
-                    .value();
+    auto* tex = context_ref.texture_pool
+                  .get(swapchain_textures.at(swapchain_current_image_index()))
+                  .value();
     return tex->get_image_view();
   }
   return VK_NULL_HANDLE;
 }
 
-Swapchain::Swapchain(IContext &ctx, uint32_t width, uint32_t height)
-    : context_ref(static_cast<Context &>(ctx)),
-      graphics_queue_handle(context_ref.get_queue(Queue::Graphics).value()),
-      swapchain_width(width), swapchain_height(height) {
-  swapchain_surface_format = choose_swap_surface_format(
-      context_ref.device_surface_formats,
-      context_ref.swapchain_requested_colour_space, true);
+Swapchain::Swapchain(IContext& ctx, uint32_t width, uint32_t height)
+  : context_ref(static_cast<Context&>(ctx))
+  , graphics_queue_handle(context_ref.get_queue(Queue::Graphics).value())
+  , swapchain_width(width)
+  , swapchain_height(height)
+{
+  swapchain_surface_format =
+    choose_swap_surface_format(context_ref.device_surface_formats,
+                               context_ref.swapchain_requested_colour_space,
+                               true);
 
   VkBool32 queue_family_supports_presentation = VK_FALSE;
   VK_VERIFY(vkGetPhysicalDeviceSurfaceSupportKHR(
-      ctx.get_physical_device(),
-      context_ref.get_queue_family_index_unsafe(Queue::Graphics),
-      context_ref.surface, &queue_family_supports_presentation));
+    ctx.get_physical_device(),
+    context_ref.get_queue_family_index_unsafe(Queue::Graphics),
+    context_ref.surface,
+    &queue_family_supports_presentation));
   assert(queue_family_supports_presentation);
 
   auto choose_swapchain_image_count =
-      [](const VkSurfaceCapabilitiesKHR &caps) -> uint32_t {
+    [](const VkSurfaceCapabilitiesKHR& caps) -> uint32_t {
     const uint32_t desired = caps.minImageCount + 1;
     const bool exceeded =
-        caps.maxImageCount > 0 && desired > caps.maxImageCount;
+      caps.maxImageCount > 0 && desired > caps.maxImageCount;
     return exceeded ? caps.maxImageCount : desired;
   };
 
   auto choose_swapchain_present_mode =
-      [](const std::vector<VkPresentModeKHR> &modes) -> VkPresentModeKHR {
+    [](const std::vector<VkPresentModeKHR>& modes) -> VkPresentModeKHR {
 #if defined(__linux__) || defined(_M_ARM64)
-    if (std::find(modes.cbegin(), modes.cend(),
+    if (std::find(modes.cbegin(),
+                  modes.cend(),
                   VK_PRESENT_MODE_IMMEDIATE_KHR) != modes.cend()) {
       return VK_PRESENT_MODE_IMMEDIATE_KHR;
     }
@@ -180,7 +213,8 @@ Swapchain::Swapchain(IContext &ctx, uint32_t width, uint32_t height)
     return VK_PRESENT_MODE_FIFO_KHR;
   };
 
-  auto choose_usage_flags = [](VkPhysicalDevice pd, VkSurfaceKHR surface,
+  auto choose_usage_flags = [](VkPhysicalDevice pd,
+                               VkSurfaceKHR surface,
                                VkFormat format) -> VkImageUsageFlags {
     VkImageUsageFlags usageFlags = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT |
                                    VK_IMAGE_USAGE_TRANSFER_DST_BIT |
@@ -193,9 +227,9 @@ Swapchain::Swapchain(IContext &ctx, uint32_t width, uint32_t height)
     vkGetPhysicalDeviceFormatProperties(pd, format, &props);
 
     const bool isStorageSupported =
-        (caps.supportedUsageFlags & VK_IMAGE_USAGE_STORAGE_BIT) > 0;
+      (caps.supportedUsageFlags & VK_IMAGE_USAGE_STORAGE_BIT) > 0;
     const bool isTilingOptimalSupported =
-        (props.optimalTilingFeatures & VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT) > 0;
+      (props.optimalTilingFeatures & VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT) > 0;
 
     if (isStorageSupported && isTilingOptimalSupported) {
       usageFlags |= VK_IMAGE_USAGE_STORAGE_BIT;
@@ -205,13 +239,14 @@ Swapchain::Swapchain(IContext &ctx, uint32_t width, uint32_t height)
   };
 
   const VkImageUsageFlags usageFlags =
-      choose_usage_flags(ctx.get_physical_device(), context_ref.surface,
-                         swapchain_surface_format.format);
+    choose_usage_flags(ctx.get_physical_device(),
+                       context_ref.surface,
+                       swapchain_surface_format.format);
   const bool is_composite_alpha_opaque_supported =
-      (context_ref.device_surface_capabilities.supportedCompositeAlpha &
-       VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR) != 0;
+    (context_ref.device_surface_capabilities.supportedCompositeAlpha &
+     VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR) != 0;
   std::array<std::uint32_t, 1> queueFamilyIndices = {
-      context_ref.get_queue_family_index_unsafe(Queue::Graphics),
+    context_ref.get_queue_family_index_unsafe(Queue::Graphics),
   };
 
   const VkSwapchainCreateInfoKHR ci = {
@@ -242,8 +277,8 @@ Swapchain::Swapchain(IContext &ctx, uint32_t width, uint32_t height)
       .clipped = VK_TRUE,
       .oldSwapchain = VK_NULL_HANDLE,
   };
-  VK_VERIFY(vkCreateSwapchainKHR(context_ref.get_device(), &ci, nullptr,
-                                 &swapchain_khr));
+  VK_VERIFY(vkCreateSwapchainKHR(
+    context_ref.get_device(), &ci, nullptr, &swapchain_khr));
 
   /*
 if (context_ref.has_EXT_hdr_metadata_) {
@@ -262,20 +297,22 @@ vkSetHdrMetadataEXT(device_, 1, &swapchain_, &metadata);
 }
 */
   std::array<VkImage, max_swapchain_images> swapchain_images;
-  VK_VERIFY(vkGetSwapchainImagesKHR(context_ref.get_device(), swapchain_khr,
-                                    &image_count, nullptr));
+  VK_VERIFY(vkGetSwapchainImagesKHR(
+    context_ref.get_device(), swapchain_khr, &image_count, nullptr));
   if (image_count > max_swapchain_images) {
     image_count = max_swapchain_images;
   }
-  VK_VERIFY(vkGetSwapchainImagesKHR(context_ref.get_device(), swapchain_khr,
-                                    &image_count, swapchain_images.data()));
+  VK_VERIFY(vkGetSwapchainImagesKHR(context_ref.get_device(),
+                                    swapchain_khr,
+                                    &image_count,
+                                    swapchain_images.data()));
 
   static constexpr auto create_semaphore = [](VkDevice device,
                                               std::string_view) -> VkSemaphore {
     const VkSemaphoreCreateInfo semaphore_info{
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-        .pNext = nullptr,
-        .flags = 0,
+      .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
+      .pNext = nullptr,
+      .flags = 0,
     };
     VkSemaphore semaphore;
     VK_VERIFY(vkCreateSemaphore(device, &semaphore_info, nullptr, &semaphore));
@@ -290,20 +327,20 @@ vkSetHdrMetadataEXT(device_, 1, &swapchain_, &metadata);
                                              "Semaphore: swapchain-acquire");
 
     VkTexture image{
-        context_ref,
-        VkTextureDescription{
-            .format = swapchain_surface_format.format,
-            .extent = {swapchain_width, swapchain_height, 1},
-            .usage_flags = TextureUsageFlags::ColourAttachment |
-                           TextureUsageFlags::TransferSource |
-                           TextureUsageFlags::TransferDestination,
-            .tiling = VK_IMAGE_TILING_OPTIMAL,
-            .initial_layout = VK_IMAGE_LAYOUT_UNDEFINED,
-            .is_owning = true,
-            .is_swapchain = true,
-            .externally_created_image = swapchain_images[i],
-            .debug_name = "Swapchain Image " + std::to_string(i),
-        },
+      context_ref,
+      VkTextureDescription{
+        .format = swapchain_surface_format.format,
+        .extent = { swapchain_width, swapchain_height, 1 },
+        .usage_flags = TextureUsageFlags::ColourAttachment |
+                       TextureUsageFlags::TransferSource |
+                       TextureUsageFlags::TransferDestination,
+        .tiling = VK_IMAGE_TILING_OPTIMAL,
+        .initial_layout = VK_IMAGE_LAYOUT_UNDEFINED,
+        .is_owning = true,
+        .is_swapchain = true,
+        .externally_created_image = swapchain_images[i],
+        .debug_name = "Swapchain Image " + std::to_string(i),
+      },
     };
 
     image.create_image_view(
@@ -336,7 +373,8 @@ vkSetHdrMetadataEXT(device_, 1, &swapchain_, &metadata);
   }
 }
 
-Swapchain::~Swapchain() {
+Swapchain::~Swapchain()
+{
   for (TextureHandle handle : swapchain_textures) {
     context().destroy(handle);
   }
@@ -350,43 +388,51 @@ Swapchain::~Swapchain() {
   }
 }
 
-auto Swapchain::swapchain_current_image_index() const -> std::uint32_t {
+auto
+Swapchain::swapchain_current_image_index() const -> std::uint32_t
+{
   return swapchain_image_index;
 }
 
-auto Swapchain::set_next_image_needed(bool val) -> void {
+auto
+Swapchain::set_next_image_needed(bool val) -> void
+{
   need_next_image = val;
 }
 
-auto Swapchain::resize(std::uint32_t , std::uint32_t ) -> void {
+auto
+Swapchain::resize(std::uint32_t, std::uint32_t) -> void
+{
   // TODO: Implement swapchain resizing
 }
 
-auto Swapchain::present(VkSemaphore wait_semaphore)
-    -> Expected<void, SwapchainPresentFailure> {
+auto
+Swapchain::present(VkSemaphore wait_semaphore)
+  -> Expected<void, SwapchainPresentFailure>
+{
 
   const VkSwapchainPresentFenceInfoEXT fence_info = {
-      .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_PRESENT_FENCE_INFO_EXT,
-      .pNext = nullptr,
-      .swapchainCount = 1,
-      .pFences = &present_fences[swapchain_image_index],
+    .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_PRESENT_FENCE_INFO_EXT,
+    .pNext = nullptr,
+    .swapchainCount = 1,
+    .pFences = &present_fences[swapchain_image_index],
   };
   const VkPresentInfoKHR pi = {
-      .sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
-      .pNext = context_ref.has_swapchain_maintenance_1 ? &fence_info : nullptr,
-      .waitSemaphoreCount = 1,
-      .pWaitSemaphores = &wait_semaphore,
-      .swapchainCount = 1u,
-      .pSwapchains = &swapchain_khr,
-      .pImageIndices = &swapchain_image_index,
-      .pResults = nullptr,
+    .sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
+    .pNext = context_ref.has_swapchain_maintenance_1 ? &fence_info : nullptr,
+    .waitSemaphoreCount = 1,
+    .pWaitSemaphores = &wait_semaphore,
+    .swapchainCount = 1u,
+    .pSwapchains = &swapchain_khr,
+    .pImageIndices = &swapchain_image_index,
+    .pResults = nullptr,
   };
 
   static auto create_fence = [](VkDevice device, const std::string_view name) {
     VkFenceCreateInfo info{
-        .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
-        .pNext = nullptr,
-        .flags = 0,
+      .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+      .pNext = nullptr,
+      .flags = 0,
     };
     info.flags = 0; // start unsignaled
     VkFence fence{};
@@ -403,13 +449,14 @@ auto Swapchain::present(VkSemaphore wait_semaphore)
   if (context_ref.has_swapchain_maintenance_1) {
     if (!present_fences[swapchain_image_index]) {
       present_fences[swapchain_image_index] =
-          create_fence(context_ref.get_device(), "Fence: present-fence");
+        create_fence(context_ref.get_device(), "Fence: present-fence");
     }
   }
   VkResult r = vkQueuePresentKHR(graphics_queue_handle, &pi);
   if (r == VK_SUBOPTIMAL_KHR || r == VK_ERROR_OUT_OF_DATE_KHR) {
     return unexpected<SwapchainPresentFailure>(
-        r == VK_ERROR_OUT_OF_DATE_KHR ? SwapchainPresentFailure::OutOfDate : SwapchainPresentFailure::Suboptimal);
+      r == VK_ERROR_OUT_OF_DATE_KHR ? SwapchainPresentFailure::OutOfDate
+                                    : SwapchainPresentFailure::Suboptimal);
   }
 
   if (r != VK_SUCCESS) {
